a0cf0a84e58eed9c4d9d71c9051721fc60bb6f3a
Jaspinder Singh <jaspinderg09@gmail.com>
2023-02-28 12:10:36 +0530

    commit added

--- Clock.java
+++ Clock.java
@@ -6,8 +6,6 @@ import java.util.Calendar;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-// Suppress serial warning
-@SuppressWarnings("serial")
 public class Clock extends JFrame {
 
     // Global Variables
@@ -27,7 +25,7 @@ public class Clock extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Java Clock");
         this.setLayout(new FlowLayout());
-        this.setSize(350, 350);
+        this.setSize(350, 250);
         this.getContentPane().setBackground(Color.BLACK);
         this.setResizable(true);
 
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Create an Instance of the MyFrame Class
+        // Create an Instance of the Class
         Clock ck = new Clock();
         ck.Clock();
     }
